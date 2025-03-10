c56714133ec742cad9098a36d0d0ee0d19612657
Viacheslav Dmitriev <slavadmitriev16@gmail.com>
2025-03-10 20:08:32 +0300

    OK code :)

--- t_test.go
+++ t_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"strings"
 	"testing"
 
@@ -11,54 +10,16 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var cafeList = map[string][]string{
-	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
-}
-
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-	countStr := req.URL.Query().Get("count")
-	if countStr == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("count missing"))
-		return
-	}
-
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong count value"))
-		return
-	}
-
-	city := req.URL.Query().Get("city")
-
-	cafe, ok := cafeList[city]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong city value"))
-		return
-	}
-
-	if count > len(cafe) {
-		count = len(cafe)
-	}
-
-	answer := strings.Join(cafe[:count], ",")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(answer))
-}
-
 func TestMainHandlerWhenOk(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
-	// проверка на пустоту
-	require.NotEmpty(t, responseRecorder.Body.String())
-	// проверка на 200 OK
+	// проверяем код ответа
 	require.Equal(t, responseRecorder.Code, http.StatusOK)
+	// проверяем не пустое ли тело
+	assert.NotEmpty(t, responseRecorder.Body)
 }
 
 func TestMainHandlerWhenWrongCity(t *testing.T) {
@@ -71,7 +32,7 @@ func TestMainHandlerWhenWrongCity(t *testing.T) {
 	bodyEcpected := "wrong city value"
 	bodyActual := responseRecorder.Body.String()
 
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
 	assert.Equal(t, bodyEcpected, bodyActual)
 }
 
@@ -82,6 +43,8 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
 
 	// здесь нужно добавить необходимые проверки
 	assert.Len(t, strings.Split(responseRecorder.Body.String(), ","), totalCount)
